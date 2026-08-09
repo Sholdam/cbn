161364f37709ecd30baed0d3fd3affe04f049872
Sholdam <guil2000super@gmail.com>
2026-08-09 12:18:11 -0300

    fix(chatwoot): preserve Vue indentation in UI patch

--- chatwoot-cbn/patches/patch_settings.rb
+++ chatwoot-cbn/patches/patch_settings.rb
@@ -5,21 +5,7 @@ replacements = {
   "      senderNameType: 'friendly',\n      businessName: ''," => "      senderNameType: 'friendly',\n      whatsappAgentNameEnabled: false,\n      businessName: '',",
   "      this.senderNameType = this.inbox.sender_name_type;\n      this.businessName = this.inbox.business_name;" => "      this.senderNameType = this.inbox.sender_name_type;\n      this.whatsappAgentNameEnabled =\n        this.inbox.sender_name_type === 'professional';\n      this.businessName = this.inbox.business_name;",
   "          sender_name_type: this.senderNameType,\n          business_name: this.businessName || null," => "          sender_name_type: this.isAWhatsAppChannel\n            ? this.whatsappAgentNameEnabled\n              ? 'professional'\n              : 'friendly'\n            : this.senderNameType,\n          business_name: this.businessName || null,",
-  <<~'OLD' => <<~'NEW'
-            <SettingsFieldSection
-              :label="$t('INBOX_MGMT.HELP_CENTER.LABEL')"
-  OLD
-            <SettingsToggleSection
-              v-if="isAWhatsAppChannel"
-              v-model="whatsappAgentNameEnabled"
-              header="Exibir nome do atendente nas mensagens"
-              description="Adiciona automaticamente o nome do agente antes das mensagens enviadas pelo WhatsApp."
-              class="mb-4"
-            />
-
-            <SettingsFieldSection
-              :label="$t('INBOX_MGMT.HELP_CENTER.LABEL')"
-  NEW
+  "            <SettingsFieldSection\n              :label=\"$t('INBOX_MGMT.HELP_CENTER.LABEL')\"" => "            <SettingsToggleSection\n              v-if=\"isAWhatsAppChannel\"\n              v-model=\"whatsappAgentNameEnabled\"\n              header=\"Exibir nome do atendente nas mensagens\"\n              description=\"Adiciona automaticamente o nome do agente antes das mensagens enviadas pelo WhatsApp.\"\n              class=\"mb-4\"\n            />\n\n            <SettingsFieldSection\n              :label=\"$t('INBOX_MGMT.HELP_CENTER.LABEL')\""
 }
 
 replacements.each do |before, after|
